24cd41a7d646aba85228d78e8820e6313d450120
Haruki <haruki@example.com>
2019-04-01 02:13:04 +0900

    Makes it truly work only in the specified URLs.

--- js/script.js
+++ js/script.js
@@ -2,17 +2,24 @@
     const PUT_TEXT = "honmaniyoMonster1 < ";
     const CHATBOX_SELECTOR = 'textarea[data-a-target="chat-input"]';
 
+    const URLs = ["https://www.twitch.tv/harukisb",
+        "https://www.twitch.tv/osushiek",
+        "https://www.twitch.tv/kirbis_044",
+        "https://www.twitch.tv/lovegorilla39",
+        "https://www.twitch.tv/ke_sapphire"
+    ];
+
     $(document).on("ready", CHATBOX_SELECTOR, function(e) {
         let $ta = $(CHATBOX_SELECTOR);
         if ($ta.val() === "") {
-            $ta.val(PUT_TEXT);
+            putText($ta);
         }
     });
 
     $(document).on("click", CHATBOX_SELECTOR, function(e) {
         let $ta = $(CHATBOX_SELECTOR);
         if ($ta.val() === "") {
-            $ta.val(PUT_TEXT);
+            putText($ta);
         }
     });
 
@@ -23,11 +30,22 @@
             } else {
                 setTimeout(function() {
                     let $ta = $(CHATBOX_SELECTOR);
-                    $ta.val(PUT_TEXT);
+                    putText($ta);
                 }, 10);
             }
         } else {
             $.noop();
         }
     });
+
+    let lastCheckedUrl = location.href;
+    function putText(textArea) {
+        for (const url of URLs) {
+            if (location.href.startsWith(url)) {
+                textArea.val(PUT_TEXT);
+                lastCheckedUrl = location.href;
+                return;
+            }
+        }
+    }
 }());
