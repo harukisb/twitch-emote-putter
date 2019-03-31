b1078dc58b899f6ce8491e5cd696e7ec5b93199a
Haruki <haruki@example.com>
2019-04-01 01:26:56 +0900

    Improved selector to select charbox only.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
-var PUT_TEXT = "honmaniyoFace < ";
+const PUT_TEXT = "honmaniyoMonster1 < ";
+const CHATBOX_SELECTOR = 'textarea[data-a-target="chat-input"]';
 
-$(document).on("click", "textarea", function(e) {
-  var $ta = $("textarea");
-  if ($ta.val() === "") {
-    $ta.val(PUT_TEXT);
-  }
+$(document).on("ready", CHATBOX_SELECTOR, function(e) {
+    let $ta = $(CHATBOX_SELECTOR);
+    if ($ta.val() === "") {
+        $ta.val(PUT_TEXT);
+    }
 });
 
-$(document).on("keydown", "textarea", function(e) {
-  if (e.keyCode == 13) { // Enterが押された
+$(document).on("click", CHATBOX_SELECTOR, function(e) {
+    let $ta = $(CHATBOX_SELECTOR);
+    if ($ta.val() === "") {
+        $ta.val(PUT_TEXT);
+    }
+});
+
+$(document).on("keydown", CHATBOX_SELECTOR, function(e) {
+  if (e.keyCode === 13) { // Enterが押された
     if (e.shiftKey) { // Shiftキーも押された
       $.noop();
     } else {
       setTimeout(function() {
-        var $ta = $("textarea");
+        let $ta = $(CHATBOX_SELECTOR);
         $ta.val(PUT_TEXT);
       }, 10);
     }
